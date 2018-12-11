e784abc63b5fa8838f5b1c20dd8cb986b3450212
Sho Amano <samano@xevo.com>
2018-12-11 12:18:39 +0900

    Add unit tests for newly added functions

--- test/bson_object_test.c
+++ test/bson_object_test.c
@@ -11,6 +11,123 @@
 #define BSON_TAG_INT32    (0x10)
 #define BSON_TAG_INT64    (0x12)
 
+START_TEST(bson_object_from_bytes_all_types)
+{
+  /*
+     {
+       "testint": 12345,
+       "testobj": {
+         "string_test": "ABCDEFGabcdefg",
+         "arr": [100, 200, 300]
+       },
+       "float": -123.456,
+       "boolValue": true
+     }
+   */
+  uint8_t test_data[] = {
+      0x86, 0x0, 0x0, 0x0, // overall length
+      BSON_TAG_INT32, 't', 'e', 's', 't', 'i', 'n', 't', 0x0,
+        0x39, 0x30, 0x0, 0x0,
+      BSON_TAG_DOCUMENT, 't', 'e', 's', 't', 'o', 'b', 'j', 0x0,
+        0x50, 0x0, 0x0, 0x0, // length of this embedded document
+        BSON_TAG_STRING, 's', 't', 'r', 'i', 'n', 'g', '_', 't', 'e', 's', 't', 0x0,
+          0x0F, 0x0, 0x0, 0x0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 0x0,
+        BSON_TAG_ARRAY, 'a', 'r', 'r', 0x0,
+          0x26, 0x0, 0x0, 0x0, // length of this embedded array
+          BSON_TAG_INT64, '0', 0x0,
+            0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+          BSON_TAG_INT64, '1', 0x0,
+            0xC8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+          BSON_TAG_INT64, '2', 0x0,
+            0x2C, 0x01, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+          0x0, // end of array
+        0x0, // end of document
+      BSON_TAG_DOUBLE, 'f', 'l', 'o', 'a', 't', 0x0,
+      0x77, 0xBE, 0x9F, 0x1A, 0x2F, 0xDD, 0x5E, 0xC0,
+      BSON_TAG_BOOLEAN, 'b', 'o', 'o', 'l', 'V', 'a', 'l', 'u', 'e', 0x0,
+        0x01, // true
+      0x0 // end of document
+  };
+
+  uint8_t *buf = malloc(sizeof(test_data));
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  memcpy(buf, test_data, sizeof(test_data));
+
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data));
+  free(buf);
+
+  ck_assert_uint_eq(ret, sizeof(test_data));
+
+  // check elements
+  BsonElement *e = bson_object_get(&output, "testint");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_INT32);
+
+  e = bson_object_get(&output, "testobj");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_DOCUMENT);
+
+  e = bson_object_get(&output, "float");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_DOUBLE);
+
+  e = bson_object_get(&output, "boolValue");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_BOOLEAN);
+
+  // check non-existing key
+  e = bson_object_get(&output, "do-not-exist");
+  ck_assert(e == NULL);
+
+  // check values
+  int32_t testint = bson_object_get_int32(&output, "testint");
+  ck_assert_int_eq(testint, 12345);
+
+  BsonObject *sub = bson_object_get_object(&output, "testobj");
+  ck_assert(sub);
+
+  double float_value = bson_object_get_double(&output, "float");
+  ck_assert(float_value == -123.456);
+
+  bson_boolean bool_value = bson_object_get_bool(&output, "boolValue");
+  ck_assert_int_eq(bool_value, BOOLEAN_TRUE);
+
+  // check inside sub object
+  e = bson_object_get(sub, "string_test");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_STRING);
+
+  e = bson_object_get(sub, "arr");
+  ck_assert(e);
+  ck_assert_int_eq(e->type, TYPE_ARRAY);
+
+  e = bson_object_get(sub, "non-existent");
+  ck_assert(e == NULL);
+
+  char *string_test = bson_object_get_string(sub, "string_test");
+  ck_assert(string_test);
+  ck_assert_str_eq(string_test, "ABCDEFGabcdefg");
+
+  BsonArray *arr = bson_object_get_array(sub, "arr");
+  ck_assert(arr);
+  ck_assert_uint_eq(arr->count, 3);
+
+  int64_t arr_value = bson_array_get_int64(arr, 0);
+  ck_assert_int_eq(arr_value, 100);
+
+  arr_value = bson_array_get_int64(arr, 1);
+  ck_assert_int_eq(arr_value, 200);
+
+  arr_value = bson_array_get_int64(arr, 2);
+  ck_assert_int_eq(arr_value, 300);
+
+  bson_object_deinitialize(&output);
+}
+END_TEST
+
 START_TEST(bson_object_from_bytes_corrupted_key)
 {
   /*
@@ -28,10 +145,11 @@ START_TEST(bson_object_from_bytes_corrupted_key)
   }
   memcpy(buf, test_data_corrupted, sizeof(test_data_corrupted));
 
-  BsonObject ret = bson_object_from_bytes(buf);
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data_corrupted));
   free(buf);
 
-  bson_object_deinitialize(&ret);
+  ck_assert_uint_eq(ret, 0);
 }
 END_TEST
 
@@ -53,10 +171,11 @@ START_TEST(bson_object_from_bytes_corrupted_tag_length)
   }
   memcpy(buf, test_data_corrupted, sizeof(test_data_corrupted));
 
-  BsonObject ret = bson_object_from_bytes(buf);
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data_corrupted));
   free(buf);
 
-  bson_object_deinitialize(&ret);
+  ck_assert_uint_eq(ret, 0);
 }
 END_TEST
 
@@ -78,10 +197,11 @@ START_TEST(bson_object_from_bytes_corrupted_string)
   }
   memcpy(buf, test_data_corrupted, sizeof(test_data_corrupted));
 
-  BsonObject ret = bson_object_from_bytes(buf);
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data_corrupted));
   free(buf);
 
-  bson_object_deinitialize(&ret);
+  ck_assert_uint_eq(ret, 0);
 }
 END_TEST
 
@@ -103,10 +223,11 @@ START_TEST(bson_object_from_bytes_corrupted_integer)
   }
   memcpy(buf, test_data_corrupted, sizeof(test_data_corrupted));
 
-  BsonObject ret = bson_object_from_bytes(buf);
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data_corrupted));
   free(buf);
 
-  bson_object_deinitialize(&ret);
+  ck_assert_uint_eq(ret, 0);
 }
 END_TEST
 
@@ -130,10 +251,11 @@ START_TEST(bson_object_from_bytes_corrupted_tag)
   }
   memcpy(buf, test_data_corrupted, sizeof(test_data_corrupted));
 
-  BsonObject ret = bson_object_from_bytes(buf);
+  BsonObject output;
+  size_t ret = bson_object_from_bytes_len(&output, buf, sizeof(test_data_corrupted));
   free(buf);
 
-  bson_object_deinitialize(&ret);
+  ck_assert_uint_eq(ret, 0);
 }
 END_TEST
 
@@ -142,6 +264,7 @@ Suite *suite(void) {
   Suite *s = suite_create("bson_object_test");
 
   TCase *tc = tcase_create("read_from_bytes");
+  tcase_add_test(tc, bson_object_from_bytes_all_types);
   tcase_add_test(tc, bson_object_from_bytes_corrupted_key);
   tcase_add_test(tc, bson_object_from_bytes_corrupted_tag_length);
   tcase_add_test(tc, bson_object_from_bytes_corrupted_string);
--- test/bson_util_test.c
+++ test/bson_util_test.c
@@ -1,25 +1,181 @@
 #include <check.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <float.h>
 #include "bson_util.h"
+
+START_TEST(read_byte)
+{
+  uint8_t buf[1] = {0xAD};
+  uint8_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_byte_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 1);
+  ck_assert_uint_eq(output, 0xAD);
+  ck_assert_ptr_eq(p, buf + 1);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_byte_overrun)
+{
+  uint8_t output = 0;
+  const uint8_t *p = NULL;
+  size_t size = 0;
+
+  size_t ret = read_byte_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 0);
+  ck_assert_ptr_eq(p, NULL);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+
+START_TEST(read_int32_le_zero)
+{
+  uint8_t buf[4] = {0, 0, 0, 0};
+  int32_t output = 12345678;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, 0);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int32_le_positive)
+{
+  uint8_t buf[5] = {0x9C, 0x92, 0xDC, 0x5F, 0x00};
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, 0x5FDC929C);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 1);
+}
+END_TEST
+
+START_TEST(read_int32_le_negative)
+{
+  uint8_t buf[4] = {0x60, 0xD1, 0x03, 0xAC};
+  int32_t expected = -(int32_t)((~((uint32_t)0xAC03D160) + 1)); // = -1409035936
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, expected);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int32_le_positive_max)
+{
+  uint8_t buf[4] = {0xFF, 0xFF, 0xFF, 0x7F};
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, INT32_MAX);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int32_le_negative_max)
+{
+  uint8_t buf[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, -1);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int32_le_negative_min)
+{
+  uint8_t buf[4] = {0, 0, 0, 0x80};
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 4);
+  ck_assert_int_eq(output, INT32_MIN);
+  ck_assert_ptr_eq(p, buf + 4);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int32_le_overrun)
+{
+  // Pass a buffer with invalid size. We use heap buffer to make valgrind detect
+  // the issue easily.
+  uint8_t *buf = malloc(2); // not enough data
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  buf[0] = buf[1] = 0;
+
+  int32_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = 2;
+
+  size_t ret = read_int32_le_len(&output, &p, &size);
+  ck_assert_int_eq(ret, 0);
+  ck_assert_ptr_eq(p, buf);
+  ck_assert_uint_eq(size, 2);
+
+  free(buf);
+}
+END_TEST
+
 
 START_TEST(read_int64_le_zero)
 {
   uint8_t buf[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
+  int64_t output = 12345678;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, 0);
   ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, 0);
+  ck_assert_uint_eq(size, 0);
 }
 END_TEST
 
 START_TEST(read_int64_le_positive)
 {
-  uint8_t buf[8] = {0x12, 0xA2, 0xBF, 0x82, 0x34, 0xD0, 0x07, 0x73};
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
+  uint8_t buf[10] = {0x12, 0xA2, 0xBF, 0x82, 0x34, 0xD0, 0x07, 0x73, 0x00, 0x00};
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, 0x7307D03482BFA212);
   ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, 0x7307D03482BFA212);
+  ck_assert_uint_eq(size, 2);
 }
 END_TEST
 
@@ -27,40 +183,283 @@ START_TEST(read_int64_le_negative)
 {
   uint8_t buf[8] = {0x34, 0xC9, 0x30, 0x74, 0x22, 0x6F, 0xCA, 0xBD};
   int64_t expected = -(int64_t)((~((uint64_t)0xBDCA6F227430C934) + 1)); // = -4770878661476693708
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, expected);
   ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, expected);
+  ck_assert_uint_eq(size, 0);
 }
 END_TEST
 
 START_TEST(read_int64_le_positive_max)
 {
   uint8_t buf[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F};
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
-  ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, INT64_MAX);
-}
-END_TEST
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
 
-START_TEST(read_int64_le_negative_min)
-{
-  uint8_t buf[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, INT64_MAX);
   ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, -1);
+  ck_assert_uint_eq(size, 0);
 }
 END_TEST
 
 START_TEST(read_int64_le_negative_max)
 {
-  uint8_t buf[8] = {0, 0, 0, 0, 0, 0, 0, 0x80};
-  uint8_t *p = buf;
-  int64_t ret = read_int64_le(&p);
+  uint8_t buf[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, -1);
   ck_assert_ptr_eq(p, buf + 8);
-  ck_assert_int_eq(ret, INT64_MIN);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int64_le_negative_min)
+{
+  uint8_t buf[8] = {0, 0, 0, 0, 0, 0, 0, 0x80};
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert_int_eq(output, INT64_MIN);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_int64_le_overrun)
+{
+  uint8_t *buf = malloc(2); // not enough data
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  buf[0] = buf[1] = 0;
+
+  int64_t output = 0;
+  const uint8_t *p = buf;
+  size_t size = 2;
+
+  size_t ret = read_int64_le_len(&output, &p, &size);
+  ck_assert_int_eq(ret, 0);
+  ck_assert_ptr_eq(p, buf);
+  ck_assert_uint_eq(size, 2);
+
+  free(buf);
+}
+END_TEST
+
+
+START_TEST(read_double_le_zero)
+{
+  uint8_t buf[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  double output = 12345678;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == 0.0);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_double_le_positive)
+{
+  uint8_t buf[9] = {0x9A, 0x99, 0x99, 0x99, 0x19, 0x74, 0x67, 0x40, 0x00};
+  double output = 123;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == 187.628125);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 1);
+}
+END_TEST
+
+START_TEST(read_double_le_negative)
+{
+  uint8_t buf[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0xA8, 0x5D, 0xC0};
+  double output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == -118.625);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_double_le_positive_max)
+{
+  uint8_t buf[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xEF, 0x7F};
+  double output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == DBL_MAX);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_double_le_positive_min)
+{
+  uint8_t buf[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x10, 0x0};
+  double output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == DBL_MIN);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_double_le_negative_min)
+{
+  uint8_t buf[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xEF, 0xFF};
+  double output = 0;
+  const uint8_t *p = buf;
+  size_t size = sizeof(buf);
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_uint_eq(ret, 8);
+  ck_assert(output == -DBL_MAX);
+  ck_assert_ptr_eq(p, buf + 8);
+  ck_assert_uint_eq(size, 0);
+}
+END_TEST
+
+START_TEST(read_double_le_overrun)
+{
+  uint8_t *buf = malloc(2); // not enough data
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  buf[0] = buf[1] = 0;
+
+  double output = 0;
+  const uint8_t *p = buf;
+  size_t size = 2;
+
+  size_t ret = read_double_le_len(&output, &p, &size);
+  ck_assert_int_eq(ret, 0);
+  ck_assert_ptr_eq(p, buf);
+  ck_assert_uint_eq(size, 2);
+
+  free(buf);
+}
+END_TEST
+
+
+START_TEST(read_string)
+{
+  const char *test_string = "ABCDEFGHIJK4567890qwerty";
+
+  size_t buf_size = strlen(test_string) + 1;
+  uint8_t *buf = malloc(buf_size);
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  memcpy(buf, test_string, strlen(test_string));
+  buf[buf_size - 1] = '\0';
+
+  const uint8_t *p = buf;
+  char *output = NULL;
+  size_t size = buf_size;
+
+  size_t ret = read_string_len(&output, &p, &size);
+  ck_assert_int_eq(ret, buf_size);
+  ck_assert_str_eq(output, test_string);
+  ck_assert_ptr_eq(p, buf + buf_size);
+  ck_assert_uint_eq(size, 0);
+
+  free(output);
+  free(buf);
+}
+END_TEST
+
+START_TEST(read_string_twice)
+{
+  const char *test_string1 = "123456";
+  const char *test_string2 = "abcdefghijklmno";
+
+  size_t buf_size = strlen(test_string1) + 1 + strlen(test_string2) + 1;
+  uint8_t *buf = malloc(buf_size);
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  memcpy(buf, test_string1, strlen(test_string1));
+  buf[strlen(test_string1)] = '\0';
+
+  memcpy(buf + strlen(test_string1) + 1, test_string2, strlen(test_string2));
+  buf[strlen(test_string1) + 1 + strlen(test_string2)] = '\0';
+
+  const uint8_t *p = buf;
+  char *output = NULL;
+  size_t size = buf_size;
+
+  size_t ret = read_string_len(&output, &p, &size);
+  ck_assert_int_eq(ret, (strlen(test_string1) + 1));
+  ck_assert_str_eq(output, test_string1);
+  ck_assert_ptr_eq(p, buf + (strlen(test_string1) + 1));
+  ck_assert_uint_eq(size, buf_size - (strlen(test_string1) + 1));
+  free(output);
+
+  ret = read_string_len(&output, &p, &size);
+  ck_assert_int_eq(ret, (strlen(test_string2) + 1));
+  ck_assert_str_eq(output, test_string2);
+  ck_assert_ptr_eq(p, buf + buf_size);
+  ck_assert_uint_eq(size, 0);
+  free(output);
+
+  free(buf);
+}
+END_TEST
+
+START_TEST(read_string_overrun)
+{
+  const char *test_string = "ABCDE";
+
+  size_t buf_size = strlen(test_string); // no '\0' at the end
+  uint8_t *buf = malloc(buf_size);
+  if (buf == NULL) {
+    ck_abort_msg("malloc failed");
+  }
+  memcpy(buf, test_string, buf_size);
+
+  const uint8_t *p = buf;
+  char *output = NULL;
+  size_t size = buf_size;
+
+  size_t ret = read_string_len(&output, &p, &size);
+  ck_assert_int_eq(ret, 0);
+  ck_assert_ptr_eq(p, buf);
+  ck_assert_uint_eq(size, buf_size);
+
+  free(buf);
 }
 END_TEST
 
@@ -69,12 +468,36 @@ Suite *suite(void) {
   Suite *s = suite_create("bson_util_test");
 
   TCase *tc = tcase_create("read_bytes");
+  tcase_add_test(tc, read_byte);
+  tcase_add_test(tc, read_byte_overrun);
+
+  tcase_add_test(tc, read_int32_le_zero);
+  tcase_add_test(tc, read_int32_le_positive);
+  tcase_add_test(tc, read_int32_le_negative);
+  tcase_add_test(tc, read_int32_le_positive_max);
+  tcase_add_test(tc, read_int32_le_negative_max);
+  tcase_add_test(tc, read_int32_le_negative_min);
+  tcase_add_test(tc, read_int32_le_overrun);
+
   tcase_add_test(tc, read_int64_le_zero);
   tcase_add_test(tc, read_int64_le_positive);
   tcase_add_test(tc, read_int64_le_negative);
   tcase_add_test(tc, read_int64_le_positive_max);
-  tcase_add_test(tc, read_int64_le_negative_min);
   tcase_add_test(tc, read_int64_le_negative_max);
+  tcase_add_test(tc, read_int64_le_negative_min);
+  tcase_add_test(tc, read_int64_le_overrun);
+
+  tcase_add_test(tc, read_double_le_zero);
+  tcase_add_test(tc, read_double_le_positive);
+  tcase_add_test(tc, read_double_le_negative);
+  tcase_add_test(tc, read_double_le_positive_max);
+  tcase_add_test(tc, read_double_le_positive_min);
+  tcase_add_test(tc, read_double_le_negative_min);
+  tcase_add_test(tc, read_double_le_overrun);
+
+  tcase_add_test(tc, read_string);
+  tcase_add_test(tc, read_string_twice);
+  tcase_add_test(tc, read_string_overrun);
 
   suite_add_tcase(s, tc);
   return s;
